0719c32f5afd02497ee9b4c2f461f50e75ced971
rqalqallaf <rq.alqallaf@gmail.com>
2022-03-21 22:55:26 +0300

    tm

--- ios-cw-5/ios-cw-5/ContentView.swift
+++ ios-cw-5/ios-cw-5/ContentView.swift
@@ -9,10 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        VStack {
+                    HStack{
+                        Image("1")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                        
+                        Text("apple")
+                            .font(.largeTitle)
+                            .bold()
+                        
+                        Spacer()
+                    }.padding(5)
+                        
+                    VStack{
+                        HStack{
+                        Image("2")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                        Image("3")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                        
+                        }
+                        HStack{
+                            
+                        Image("4")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                        Image("5")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                        }
+                        
+                        
+                    }
+                }
+            }
+        }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
